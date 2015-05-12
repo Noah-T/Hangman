66a3a36c88d8921a22c926264dd2ff1014f9ec9e
Noah Teshu <nteshu@gmail.com>
2015-05-12 15:31:28 -0400

    add interface for guessing letters

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -5,6 +5,8 @@
  */
 package hangman;
 
+import java.io.IOException;
+
 /**
  *
  * @author noahteshu
@@ -14,10 +16,16 @@ public class Hangman {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         Game game = new Game("Noah's Game");
-        
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit) {
+            System.out.println("We got a hit!");
+        } else {
+            System.out.println("That's a miss.");
+        }
     }
 
 }
--- src/hangman/Prompter.java
+++ src/hangman/Prompter.java
@@ -5,10 +5,29 @@
  */
 package hangman;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author noahteshu
  */
 public class Prompter {
+    private Game mGame;
+    
+    public Prompter (Game game){
+        mGame = game;
+    }
+    
+    public boolean promptForGuess() throws IOException{
+        BufferedReader theReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a letter:  ");
+        String answer = theReader.readLine();
+     
+        char guess = answer.charAt(0);
+        return mGame.applyGuess(guess);
+
+    }
     
 }
